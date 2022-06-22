b6dc31774df446f3c0d592207b2c7f847475f26f
mnema <mnema@umich.edu>
2022-06-22 17:58:08 -0400

    api1

--- app/__init__.py
+++ app/__init__.py
@@ -98,7 +98,7 @@ def post_time_line_post():
     return model_to_dict(timeline_post)
 
 @app.route('/api/timeline_post', methods=['GET'])
-def get_time_line_post:
+def get_time_line_post():
     return {
         'timeline_posts': [
             model_to_dict(p)
