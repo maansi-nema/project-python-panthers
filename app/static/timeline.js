d093a9fe8e02ce5eb3755f8fcf6bd985a06b1d11
mnema <mnema@umich.edu>
2022-06-27 19:40:07 -0400

    back to original

--- app/static/timeline.js
+++ app/static/timeline.js
@@ -1,7 +1,5 @@
 fetch('/api/timeline_post').then(response => {
-            alert("hello");
             console.log(response);
-            alert(response);
             if (!response.ok){
                 throw Error("ERROR");
             }
@@ -19,20 +17,17 @@ fetch('/api/timeline_post').then(response => {
             
                 `;
             }).join('');
-                alert("hello3");
                 console.log(html);
                 document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
         }).catch(error => {
             console.log(error);
         });
-        alert("hello4");
         const form = document.getElementById('timeline_form');
         form.addEventListener('submit', function(e) {
             e.preventDefault();
 
             const payload = new FormData(form);
            console.log([...payload])
-            alert("hello5");
 
             fetch('/api/timeline_post', {
                 method: 'POST',
@@ -40,7 +35,9 @@ fetch('/api/timeline_post').then(response => {
             })
             
             // .then(res => res.json())
+            
             .then(data => console.log(data))
             .catch(err => console.log(err))
-            alert("hello6");
+            form.reset()
+            location.reload()
         });
