48647d2c2c2d02ee84a9ce1bce1ca29d63b3beec
mnema <mnema@umich.edu>
2022-06-27 19:31:19 -0400

    back to original cod454342

--- app/static/timeline.js
+++ app/static/timeline.js
@@ -40,7 +40,7 @@ fetch('/api/timeline_post').then(response => {
             })
             
             .then(res => res.json())
-            //.then(data => console.log(data))
-           // .catch(err => console.log(err))
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
             alert("hello6");
         });
