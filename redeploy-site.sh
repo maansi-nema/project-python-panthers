275563e09534e634be825b1f72702bb897421c26
mnema <mnema@umich.edu>
2022-07-20 18:02:43 -0400

    lol

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,26 +1,8 @@
 
 #!/bin/bash
 
-# Kill existing tmux sessions
-# tmux kill-server
-
-# Go into folder
-# cd project-python-panthers
-
-# Make sure site is up to date from VCS
 git fetch && git reset origin/main --hard
 
 docker compose -f docker-compose.prod.yml down
 
 docker compose -f docker-compose.prod.yml up -d --build
-
-# Enter python venv and install dependencies
-# source python3-virtualenv/bin/activate
-
-# Install requirements
-# pip install -r requirements.txt
-
-# Start Flask in a detached tmux session
-# tmux new-session -d -s Flask
-# tmux send-keys 'source python3-virtualenv/bin/activate' C-m
-# tmux send-keys 'flask run --host=0.0.0.0' C-m
