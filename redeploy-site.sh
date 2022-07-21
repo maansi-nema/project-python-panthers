0bd34ef9eeba9f5d62feb77a457a680abe351fae
mnema <mnema@umich.edu>
2022-07-20 22:12:39 -0400

    hope this works

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo "0"
+cd project-python-panthers
 git fetch && git reset origin/main --hard
-echo "1"
 docker compose -f docker-compose.prod.yml down
-echo "2"
 docker compose -f docker-compose.prod.yml up -d --build
-echo "3"
